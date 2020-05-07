9db37900225937ae9ecd81195507b7b5da9f4241
Sergey <kvapuluk71@gmail.com>
2020-05-07 20:20:24 +0300

    Add Article class

---
+++ lab5/Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public enum StatusUnum { CONFIRMED, ERROR, PENDING }
+    
+    class Article
+    {
+        public DateTime? date;
+        public String id { get; set; }
+        public String gtin { get; set; }
+        public String lot { get; set; }
+        public Double quantity { get; set; }
+        public String subsys_art_no { get; set; }
+        public Double volume { get; set; }
+        public String vetDoc { get; set; }
+        public String unit { get; set; }
+        public StatusUnum status { get; set; }
+        public String errorMessage { get; set; }
+
+        public Article()
+        {
+            id = "1";
+        }
+    }
+}
